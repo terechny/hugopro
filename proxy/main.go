b278b95ce89f818f051392a4466c5fb65afef764
ars <terechny@gmail.com>
2023-11-29 20:51:27 +0300

    task proxy FIX

--- proxy/main.go
+++ proxy/main.go
@@ -7,10 +7,11 @@ import (
 	"net/http/httputil"
 	"net/url"
 	"os"
+	"strings"
 	"time"
 
+	"github.com/go-chi/chi"
 	"github.com/go-chi/chi/middleware"
-	"github.com/go-chi/chi/v5"
 )
 
 func main() {
@@ -18,18 +19,12 @@ func main() {
 	r := chi.NewRouter()
 	r.Use(middleware.Logger)
 
-	r.Get("/api/*", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprint(w, "Hello from API")
+	nextHandler := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("Hello from API"))
 	})
 
-	r.Handle("/*", http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		u, _ := url.Parse("http://hugo:1313")
-		r.URL.Scheme = u.Scheme
-		r.URL.Host = u.Host
-		r.URL.Path = u.Path
-		proxy := httputil.NewSingleHostReverseProxy(u)
-		proxy.ServeHTTP(w, r)
-	}))
+	rp := NewReverseProxy("hugo", "1313")
+	r.Handle("/*", rp.ReverseProxy(nextHandler))
 
 	http.ListenAndServe(":8080", r)
 }
@@ -47,15 +42,32 @@ func NewReverseProxy(host, port string) *ReverseProxy {
 }
 
 func (rp *ReverseProxy) ReverseProxy(next http.Handler) http.Handler {
+
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 
+		if r.URL.Path == "/api" || strings.HasPrefix(r.URL.Path, "/api/") {
+
+			next.ServeHTTP(w, r)
+
+		} else {
+
+			u, _ := url.Parse("http://hugo:1313")
+			r.URL.Scheme = u.Scheme
+			r.URL.Host = u.Host
+			r.URL.Path = u.Path
+			proxy := httputil.NewSingleHostReverseProxy(u)
+			proxy.ServeHTTP(w, r)
+
+		}
+
 	})
+
 }
 
 const content = ``
 
 func WorkerTest() {
-	t := time.NewTicker(1 * time.Second)
+	t := time.NewTicker(5 * time.Second)
 	var b byte = 0
 	for {
 		select {
@@ -68,3 +80,28 @@ func WorkerTest() {
 		}
 	}
 }
+
+/*
+func Worker() {
+	t := time.NewTicker(5 * time.Second)
+	var counter int
+	for {
+		select {
+		case <-t.C:
+
+			currentTime := time.Now().Format("2006-01-02 15:04:05")
+
+			fmt.Println(currentTime, counter)
+
+			err := os.WriteFile("/app/static/tasks/_index.md", []byte(fmt.Sprintf("Текущее время: %s\n\nСчетчик: %d", currentTime, counter)), 0644)
+
+			if err != nil {
+				log.Println(err)
+			}
+
+			counter++
+		}
+	}
+}
+
+*/
